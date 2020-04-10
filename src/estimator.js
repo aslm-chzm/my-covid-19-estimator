b20a13e9c5761c03bf3cf7630bf989386b9d253c
aslm <63300174+aslm-chzm@users.noreply.github.com>
2020-04-10 23:30:41 +0000

    Build Estimator

--- src/estimator.js
+++ src/estimator.js
@@ -10,7 +10,7 @@ const covid19ImpactEstimator = (data) => {
   const impact = {};
   const severeImpact = {};
 
-  // Challenge 1 Complete
+  // Challenge 1
   const calculateLockDownDays = () => {
     switch (periodType) {
       case 'months':
@@ -30,17 +30,20 @@ const covid19ImpactEstimator = (data) => {
   impact.currentlyInfected = reportedCases * 10;
   severeImpact.currentlyInfected = reportedCases * 50;
   impact.infectionsByRequestedTime = calculateInfectionByTime(impact.currentlyInfected);
-  severeImpact.infectionsByRequestedTime = calculateInfectionByTime(severeImpact.currentlyInfected);
+  severeImpact.infectionsByRequestedTime = calculateInfectionByTime(
+    severeImpact.currentlyInfected
+  );
 
   // Challenge 2
   const calculateSevereCases = (infectionByTime) => infectionByTime * 0.15;
   const calculateHospitalBeds = (totalBeds,
-    casesByThatTime) => (totalBeds * 0.35) - casesByThatTime;
+    casesByThatTime) => Math.floor((totalBeds * 0.35) - casesByThatTime);
 
   impact.severeCasesByRequestedTime = calculateSevereCases(impact.infectionsByRequestedTime);
   severeImpact.severeCasesByRequestedTime = calculateSevereCases(
     severeImpact.infectionsByRequestedTime
   );
+
   impact.hospitalBedsByRequestedTime = calculateHospitalBeds(totalHospitalBeds,
     impact.severeCasesByRequestedTime);
   severeImpact.hospitalBedsByRequestedTime = calculateHospitalBeds(totalHospitalBeds,
@@ -48,14 +51,19 @@ const covid19ImpactEstimator = (data) => {
 
   // Challenge 3
   function calculateEconomyLoss(casesByThatTime) {
-    return casesByThatTime * avgDailyIncomeInUSD * avgDailyIncomePopulation * lockDownDays;
+    return Math.floor(
+      (casesByThatTime * avgDailyIncomeInUSD * avgDailyIncomePopulation) / lockDownDays
+    );
   }
 
-  impact.casesForICUByRequestedTime = impact.infectionsByRequestedTime * 0.05;
-  severeImpact.casesForICUByRequestedTime = severeImpact.infectionsByRequestedTime * 0.05;
+  impact.casesForICUByRequestedTime = Math
+    .floor(impact.infectionsByRequestedTime * 0.05);
+  severeImpact.casesForICUByRequestedTime = Math
+    .floor(severeImpact.infectionsByRequestedTime * 0.05);
 
-  impact.casesForVentilatorsByRequestedTime = impact.infectionsByRequestedTime * 0.02;
-  severeImpact.casesForVentilatorsByRequestedTime = severeImpact.infectionsByRequestedTime * 0.02;
+  impact.casesForVentilatorsByRequestedTime = Math.floor(impact.infectionsByRequestedTime * 0.02);
+  severeImpact.casesForVentilatorsByRequestedTime = Math
+    .floor(severeImpact.infectionsByRequestedTime * 0.02);
 
   impact.dollarsInFlight = calculateEconomyLoss(impact.infectionsByRequestedTime);
   severeImpact.dollarsInFlight = calculateEconomyLoss(severeImpact.infectionsByRequestedTime);
@@ -68,4 +76,50 @@ const covid19ImpactEstimator = (data) => {
   };
 };
 
+// const data = {
+//     region: {
+//         name: 'Africa',
+//         avgAge: 19.7,
+//         avgDailyIncomeInUSD: 5,
+//         avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: 'days',
+//     timeToElapse: 60,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+// };
+// const data1 = {
+//     region: {
+//         name: 'Africa',
+//         avgAge: 19.7,
+//         avgDailyIncomeInUSD: 5,
+//         avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: 'weeks',
+//     timeToElapse: 8.571428571,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+// };
+// const data2 = {
+//     region: {
+//         name: 'Africa',
+//         avgAge: 19.7,
+//         avgDailyIncomeInUSD: 5,
+//         avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: 'months',
+//     timeToElapse: 0.35,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+// };
+
+// console.log(covid19ImpactEstimator(data));
+// console.log(covid19ImpactEstimator(data1));
+// console.log(covid19ImpactEstimator(data2));
+
+
+// Comment this and uncomment the above comments for local testing
 export default covid19ImpactEstimator;
